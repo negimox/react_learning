6c9c0ed21684999970a7fca5df980d5673d42a25
negimox <bucky.pandey@gmail.com>
2024-01-02 21:16:12 +0530

    Finished Lec02

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-const 
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("h1",{},"React hello");
+
+console.log(heading);
+const jsxHeading = <h1 id="head">Hello JSX.</h1>;
+console.log(jsxHeading);
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+
+
